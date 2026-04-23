b9c61bc3c641cdbd21a899ea972a2785bbaaf86f
Kevin GALLET <kevingallet@gmail.com>
2026-04-23 11:57:56 +0200

    chore: use proconnect button from DSFR

--- apps/web/src/features/authentification/components/proconnect-button.tsx
+++ apps/web/src/features/authentification/components/proconnect-button.tsx
@@ -1,5 +1,5 @@
+import { ProConnectButton as DsfrProConnectButton } from "@codegouvfr/react-dsfr/ProConnectButton";
 import { authClient } from "~/lib/auth-client";
-import styles from "./proconnect-button.module.css";
 
 type ProConnectButtonProps = {
   callbackURL?: string;
@@ -13,24 +13,5 @@ export function ProConnectButton({ callbackURL = "/onboarding" }: ProConnectButt
     });
   };
 
-  return (
-    <div>
-      <button type="button" onClick={handleClick} className={styles.button}>
-        <span aria-hidden="true" className={styles.badge}>
-          PRO
-        </span>
-        S'identifier avec ProConnect
-      </button>
-      <div className="fr-mt-1w">
-        <a
-          href="https://www.proconnect.gouv.fr/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="fr-link fr-link--sm"
-        >
-          Qu'est-ce que ProConnect ?
-        </a>
-      </div>
-    </div>
-  );
+  return <DsfrProConnectButton onClick={handleClick} />;
 }
